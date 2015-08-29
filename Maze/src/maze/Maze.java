734d16c5a08418e090bf5cd04f583413f536e19e
DnlRKorn <dnlrkorn@gmail.com>
2015-08-29 17:49:02 -0400

    Added vertex

--- Maze/src/maze/Maze.java
+++ Maze/src/maze/Maze.java
@@ -8,14 +8,10 @@ import java.io.IOException;
 public class Maze {
 
 	private boolean[][] mazeArray;
-	public int goalX;
-	public int goalY;
-	public int startX;
-	public int startY;
+	public Vertex goal;
+	public Vertex start;
 	
 	public Maze(String fileLoc){
-//		Scanner s = new Scanner(System.in);
-//		while
 		BufferedReader br = null;
 		try {
 			br = new  BufferedReader (new FileReader(fileLoc) );
@@ -23,30 +19,29 @@ public class Maze {
 			e.printStackTrace();
 		}
 		try {
+			
 			while(br.readLine()!=null){
-				
+				System.out.println();
 			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} catch (IOException e) {}
 		
 	}
 	
-	public boolean canMoveLeft(int x, int y){
-		return mazeArray[x-1][y];
+	public boolean canMoveLeft(Vertex v){
+		return mazeArray[v.x-1][v.y];
 	}
 	
-	public boolean canMoveRight(int x, int y){
-		return mazeArray[x+1][y];
+	public boolean canMoveRight(Vertex v){
+		return mazeArray[v.x+1][v.y];
 	}
 	
-	public boolean canMoveDown(int x, int y){
-		return mazeArray[x][y-1];
+	public boolean canMoveDown(Vertex v){
+		return mazeArray[v.x][v.y-1];
 	}
 	
-	public boolean canMoveUp(int x, int y){
-		return mazeArray[x][y+1];
+	public boolean canMoveUp(Vertex v){
+		return mazeArray[v.x][v.y+1];
 	}
+	
 	
 }
